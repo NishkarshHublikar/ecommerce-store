f92f9dc1d026ca42165840467c5fd46267c2039d
Nishkarsh Hublikar <nishkarshhublikar@gmail.com>
2026-03-10 22:14:15 +0530

    feat: added cart and cartItem tables to database

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,15 +1,41 @@
-type Props={
-    params:{
-        productId: string
-    }
-}
-export default function ProductDetailPage({params}:Props){
-    return(
-        <div className ="p-10">
-            <h1 className ="text-3xl font-bold mb-6">
-                Product ID: {params.productId}
-            </h1>
-            <p>This is the product details page.</p> 
-        </div>
-    )
+import { prisma } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: Promise<{
+    productId: string;
+  }>;
+};
+
+export default async function ProductDetailPage({ params }: Props) {
+    const { productId } = await params;
+    const product = await prisma.product.findUnique({
+    where: {
+      id: Number(productId),
+    },
+  });
+
+  if (!product) return notFound();
+
+  return (
+    <div className="p-10 grid grid-cols-1 md:grid-cols-2 gap-10">
+      <img
+        src={product.image}
+        alt={product.name}
+        className="w-full rounded-lg"
+      />
+
+      <div>
+        <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
+
+        <p className="text-xl text-gray-600 mb-6">
+          ${product.price}
+        </p>
+
+        <button className="bg-black text-white px-6 py-3 rounded">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 }
