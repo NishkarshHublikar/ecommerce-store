887759a8f377e4af9eaa28b267713c8874da7544
Nishkarsh Hublikar <nishkarshhublikar@gmail.com>
2026-03-10 11:57:52 +0530

    feat : added additional product cards to product grid.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,46 @@
 import ProductCard from "@/components/ProductCard";
-export default function ProductsPage(){
-    return(
-        <div className ="p-10 grid grid-cols-3 gap-6">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-        </div>
-    );
+
+export default function ProductsPage() {
+  return (
+    <div className="p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+
+        <ProductCard
+            name="Classic Watch"
+            price={149}
+            image="https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+        />
+
+        <ProductCard
+            name="Modern Lamp"
+            price={299}
+            image="https://images.unsplash.com/photo-1503602642458-232111445657"
+        />
+
+        <ProductCard
+            name="Elegant Chair"
+            price={199}
+            image="https://images.unsplash.com/photo-1519710164239-da123dc03ef4"
+        />
+
+        <ProductCard
+            name="Minimalist Table"
+            price={399}
+            image="https://images.unsplash.com/photo-1555041469-a586c61ea9bc"
+        />
+
+
+        <ProductCard 
+            name ="Smartphone Pro"
+            price={699}
+            image="https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+        />
+        
+        <ProductCard   
+            name="Amazon Echo "
+            price ={299}
+            image="https://images.unsplash.com/photo-1518444065439-e933c06ce9cd"
+        />
+
+    </div>
+  );
 }
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,21 +1,26 @@
-export default function ProductCard() {
+type ProductCardProps={
+    name : string;
+    price : number;
+    image : string;
+}
+export default function ProductCard({name,price, image}: ProductCardProps) {
   return (
     <div className="border rounded-lg overflow-hidden shadow-sm hover:shadow-lg transition duration-300">
       {/* Product Image */}
       <img
-        src="https://images.unsplash.com/photo-1523275335684-37898b6baf30"
-        alt="product"
-        className="w-full h-56 object-cover"
+        src={image}
+        alt={name}
+        className="w-full h-60 object-cover"
       />
 
       {/* Product Info */}
       <div className="p-4">
         <h2 className="text-lg font-semibold">
-          Classic Watch
+          {name}
         </h2>
 
         <p className="text-gray-600 mt-1">
-          $149
+          Rs. {price}
         </p>
 
         {/* Button */}
